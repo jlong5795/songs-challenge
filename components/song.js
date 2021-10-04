26554c06db7bdd168d730d6c62da4d9f329727b6
Jason Long <jlong5795@gmail.com>
2021-10-03 19:32:06 -0500

    Completed style updates

--- components/song.js
+++ components/song.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Song({ song }) {
   return (
     <div className="col">
-      <div className="card">
+      <div className="card h-100 shadow-sm">
         {song.album_cover_art_url && (
           <Image
             src={song.album_cover_art_url}
@@ -16,8 +16,23 @@ export default function Song({ song }) {
 
         <div className="card-body">
           <p className="card-text">{song.track_name}</p>
-          <small className="text-black-50">{song.track_artist}</small>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <small className="text-muted">{song.track_artist}</small>
+            {song.danceability > .6 ? <span className="badge bg-success">Danceable!</span> : null}
+          </div>
         </div>
+        <span>
+          {song?.playlist_genre ? (
+            <div className="font-weight-light text-capitalize border-top">
+              {song.playlist_genre}
+            </div>
+          ) : null}
+          {song?.playlist_subgenre ? (
+            <div className="font-weight-light text-capitalize border-top">
+              {song.playlist_subgenre}
+            </div>
+          ) : null}
+        </span>
       </div>
     </div>
   );
